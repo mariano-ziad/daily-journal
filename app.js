5791a898ce5ca82cd8e3f1241a1ae08c3a92f6d3
ziad <ziadmoo1999@gmail.com>
2024-03-15 17:00:30 +0200

    added dailyblogDB

--- app.js
+++ app.js
@@ -2,61 +2,84 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
+const mongoose = require("mongoose");
+//////////////////////////////////////////////////////////////////////////////////
+const homeStartingContent = "Welcome to your daily blog.";
+const aboutContent = "I'm a junior full stack web developer.";
+const contactContent = "contact me.";
+//////////////////////////////////////////////////////////////////////////////////
 const app = express();
-let posts = [];
-
-
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+//////////////////////////////////////////////////////////////////////////////////
+mongoose.connect(
+  "mongodb+srv://Ziad:y5y6y7y8@my-first-cluster.hifjeup.mongodb.net/dailyblogDB"
+);
+const blogSchema = mongoose.Schema({
+  header: String,
+  subject: String,
+});
+const Blog = mongoose.model("Blog", blogSchema);
+//////////////////////////////////////////////////////////////////////////////////
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
-app.get("/", (req, res)=>{
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts
-  });
+//////////////////////////////////////////////////////////////////////////////////
+app.get("/", (req, res) => {
+  Blog.find({})
+    .then((blog) => {
+      res.render("home", {
+        homeStartingContent: homeStartingContent,
+        posts: blog,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-
-app.get("/about", (req, res)=>{
-  res.render("about", {aboutContent: aboutContent})
+//////////////////////////////////////////////////////////////////////////////////
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
 });
-
-app.get("/contact", (req, res)=>{
-  res.render("contact", {contactContent: contactContent})
+//////////////////////////////////////////////////////////////////////////////////
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
 });
-
-app.get("/compose", (req, res)=>{
-  res.render("compose")
+//////////////////////////////////////////////////////////////////////////////////
+app.get("/compose", (req, res) => {
+  res.render("compose");
 });
+//////////////////////////////////////////////////////////////////////////////////
 app.get("/posts/:postPage", (req, res) => {
   let postName = _.lowerCase(_.kebabCase(req.params.postPage));
-  posts.forEach(function(post) {
-    let postTitle = _.lowerCase(_.kebabCase(post.title));
-    if (postName === postTitle) {
-      res.render("post", {
-        postTitle: post.title,
-        postContent: post.content
+  Blog.find({})
+    .then((posts) => {
+      posts.forEach((post) => {
+        let postTitle = _.lowerCase(_.kebabCase(post.header));
+        if (postName === postTitle) {
+          res.render("post", {
+            postTitle: post.header,
+            postContent: post.subject,
+          });
+        }
       });
-      return;
-    }
-  });
-  res.status(404).send("Post not found");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-app.post("/compose", (req, res)=>{
+//////////////////////////////////////////////////////////////////////////////////
+app.post("/compose", (req, res) => {
   const post = {
     title: req.body.newComposeTitle,
-    content: req.body.newComposeContent
+    content: req.body.newComposeContent,
   };
-  posts.push(post);
-  res.redirect("/")
-
+  const blog = new Blog({
+    header: req.body.newComposeTitle,
+    subject: req.body.newComposeContent,
+  });
+  blog.save();
+  res.redirect("/");
 });
-app.listen(3000 || process.env.PORT , function() {
+//////////////////////////////////////////////////////////////////////////////////
+app.listen(3000 || process.env.PORT, function () {
   console.log("Server started on port 3000");
 });
